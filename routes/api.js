aa74c53cbe747f89be76ca8ae51566863e6832e2
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-12-22 15:33:25 +0700

    fix 1: add db to api

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const createCRUDRoute = require("../crudGenerator");
+const db = require("./../db");
 
 // sleepData
 router.use(
